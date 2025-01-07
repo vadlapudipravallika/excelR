7d3beffa8f508f7b10da126279d4f3959b8c47ee
vadlapudipravallika <2311cs010665@mallareddyuniversity.ac.in>
2025-01-07 19:28:35 +0530

    unary operators

---
+++ demo14.java
@@ -0,0 +1,20 @@
+public class demo14{
+    public static void main(String[] args) {
+        int x = 9;
+        System.out.println("preincrement of x: " + ++x);
+        System.out.println("x = " + x);
+
+        int y = 8;
+        System.out.println("post-increment of y: " + y++);
+        System.out.println("y = " + y);
+
+        int z = 7;
+        System.out.println("pre=decrement of z: " + --z);
+        System.out.println("z = " + z);
+
+        int a = 6;
+        System.out.println("post-decrement of a: " + a--);
+        System.out.println("a = " + a);
+
+    }
+}
