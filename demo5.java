2a0954eb89db0f57db0fda6476d4273af957d0dc
vadlapudipravallika <2311cs010665@mallareddyuniversity.ac.in>
2025-01-07 19:23:10 +0530

    check credentials

---
+++ demo5.java
@@ -0,0 +1,10 @@
+public class demo5{
+    public static void main(String[] args) {
+        String username = "user001";
+        String password = "user_001";
+        while((username == "user001") && (password == "user_001")) { 
+            System.out.println("hello happy new year");
+            break;
+        }
+    }
+}
