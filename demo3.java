2fe6a70e243d1550026834e628d73fb31d4674e4
vadlapudipravallika <2311cs010665@mallareddyuniversity.ac.in>
2025-01-07 19:21:22 +0530

    greatest of 3 numbers

---
+++ demo3.java
@@ -0,0 +1,16 @@
+public class demo3{
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+        int c = 2;
+        if (a>b) {
+            System.err.println("a is greatesst");
+        }
+        else if (b>c) {
+            System.err.println("b is greatest");
+        }
+        else {
+            System.err.println("c is greatest");
+        }
+    }
+}
