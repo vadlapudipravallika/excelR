69c2db8bab77b5a6dc302b3ca55df9159730452a
vadlapudipravallika <2311cs010665@mallareddyuniversity.ac.in>
2025-01-20 10:25:07 +0530

    array list

---
+++ demo029.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class demo029 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("pravallika");
+        names.add("vallika");
+        names.add("ravallika");
+        names.add("pravalli");
+        names.add("valli");
+        names.add("ravalli");
+        names.add("avallika");
+        names.add("avalli");
+        names.add("ali");
+        names.add("praval");
+        System.out.println("names of my friends: " + names);
+
+    }
+}
