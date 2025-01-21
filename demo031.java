6d1647303bc4d34ddbadc41d8d8a38826f5e194b
vadlapudipravallika <2311cs010665@mallareddyuniversity.ac.in>
2025-01-21 09:52:18 +0530

    Hash set

---
+++ demo031.java
@@ -0,0 +1,24 @@
+//Demo031.java Hash Set - Collections 
+import java.util.HashSet;
+class HashSetDemo1 {
+	void HSetMethod() {
+		//We can add any type of element here...
+		HashSet hashSet = new HashSet();
+		hashSet.add(100);
+		hashSet.add("PRAVALLIKA");
+		hashSet.add(9999.99);
+		System.out.println(hashSet);		
+//we can use Object class to read any type of elements
+		for(Object x: hashSet) {
+			System.out.println(x);
+		}
+	}
+}
+
+
+public class demo031 {
+	public static void main(String[] args) {
+		HashSetDemo1 obj = new HashSetDemo1();
+		obj.HSetMethod();
+		}
+	}
