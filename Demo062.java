7163d0ff658024df5c44260ab1d9f0e4326ca360
vadlapudipravallika <2311cs010665@mallareddyuniversity.ac.in>
2025-02-05 09:55:13 +0530

    1st 2nd last element of array

---
+++ Demo062.java
@@ -0,0 +1,15 @@
+public class Demo062 {
+        public static void main(String[] args) {
+            int[] arr = {10, 20, 30, 40, 50}; // Example array
+    
+            // Ensure the array has at least two elements
+            if (arr.length >= 2) {
+                System.out.println("First Element: " + arr[0]);
+                System.out.println("Second Element: " + arr[1]);
+                System.out.println("Last Element: " + arr[arr.length - 1]);
+            } else {
+                System.out.println("Array must have at least two elements.");
+     }
+    }
+    }
+
