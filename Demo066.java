6690e0073665eb9af22d3ec2a04b8b58da4a3f1b
vadlapudipravallika <2311cs010665@mallareddyuniversity.ac.in>
2025-02-05 10:14:01 +0530

    index values of array

---
+++ Demo066.java
@@ -0,0 +1,10 @@
+public class Demo066 {
+        public static void main(String[] args) {
+            int[] arr = {10, 20, 30, 40, 50}; // Example array
+    
+            System.out.println("Index : Element");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(i + " : " + arr[i]);
+            }
+        }
+    }
